cb0e67bad765502886f037664b3c050d7a60ec49
zhongtianshan <pythonishelloword@outlook.com>
2026-07-24 17:19:00 +0800

    更新索引

--- data.js
+++ data.js
@@ -1,29 +1,10 @@
 var blogData = {
-  "posts": [
-    {
-      "title": "测试",
-      "date": "未知日期",
-      "tags": [],
-      "file": "测试.md",
-      "content": "! \\[链接](https://www.baidu.com)(百度)",
-      "preview": "! \\链接(百度)..."
-    },
-    {
-      "title": "链接测试",
-      "date": "2026-07-24",
-      "tags": [
-        "测试"
-      ],
-      "file": "test-link.md",
-      "content": "自定义链接语法测试：\n\n！[链接](https://www.baidu.com)(百度)\n！[链接](https://github.com)(GitHub)\n！[链接](https://www.bilibili.com)(B站)\n\n普通 markdown 链接是绿色：\n[百度](https://www.baidu.com)",
-      "preview": "自定义链接语法测试：  ！链接(百度) ！链接(GitHub) ！链接(B站)  普通 markdown 链接是绿色： 百度..."
-    }
-  ],
+  "posts": [],
   "images": [
     {
       "name": "山水.jpg",
       "path": "images/yingxiang/山水.jpg"
     }
   ],
-  "updated": "2026-07-24T17:11:20.503912"
+  "updated": "2026-07-24T17:19:00.441486"
 };
