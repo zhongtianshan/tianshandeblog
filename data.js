e93bcb692f7d8bd0cb7b52b9190f835606650fb3
zhongtianshan <pythonishelloword@outlook.com>
2026-07-24 17:57:37 +0800

    更新索引

--- data.js
+++ data.js
@@ -1,24 +1,11 @@
 var blogData = {
-  "posts": [
-    {
-      "title": "原神",
-      "date": "2026-07-24",
-      "tags": [
-        "原神"
-      ],
-      "file": "test-link.md",
-      "content": "## 原神牛逼",
-      "preview": "原神牛逼..."
-    }
-  ],
+  "posts": [],
   "images": [
     {
       "name": "山水.jpg",
       "path": "images/yingxiang/山水.jpg"
     }
   ],
-  "tags": [
-    "原神"
-  ],
-  "updated": "2026-07-24T17:56:28.744420"
+  "tags": [],
+  "updated": "2026-07-24T17:57:37.015828"
 };
