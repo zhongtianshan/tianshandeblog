2f560ca1405d63b6b5621954f277b6e38b55c93e
zhongtianshan <pythonishelloword@outlook.com>
2026-07-24 16:20:32 +0800

    影像库图片重命名：landscape-1 → 山水

--- data.js
+++ data.js
@@ -2,9 +2,9 @@ var blogData = {
   "posts": [],
   "images": [
     {
-      "name": "landscape-1.jpg",
-      "path": "images/yingxiang/landscape-1.jpg"
+      "name": "山水.jpg",
+      "path": "images/yingxiang/山水.jpg"
     }
   ],
-  "updated": "2026-07-24T08:15:41.590Z"
+  "updated": "2026-07-24T08:20:12.232Z"
 };
