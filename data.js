705932471a46a4f738d99e884575451b905b6a01
zhongtianshan <pythonishelloword@outlook.com>
2026-07-24 17:00:36 +0800

    删除测试文章

--- data.js
+++ data.js
@@ -1,22 +1,10 @@
 var blogData = {
-  "posts": [
-    {
-      "title": "你好世界",
-      "date": "2026-07-24",
-      "tags": [
-        "测试",
-        "第一篇文章"
-      ],
-      "file": "hello-world.md",
-      "content": "## 你好！\n\n这是我的第一篇博客文章，用 Markdown 写的。\n\n### 功能测试\n\n- **加粗** 和 *斜体* \n- `代码块`\n- [百度](https://www.baidu.com)\n\n> 赛博朋克风格博客",
-      "preview": "你好！  这是我的第一篇博客文章，用 Markdown 写的。  功能测试  - 加粗 和 斜体  - 代码块 - 百度  > 赛博朋克风格博客..."
-    }
-  ],
+  "posts": [],
   "images": [
     {
       "name": "山水.jpg",
       "path": "images/yingxiang/山水.jpg"
     }
   ],
-  "updated": "2026-07-24T16:59:52.515437"
+  "updated": "2026-07-24T17:00:36.301165"
 };
