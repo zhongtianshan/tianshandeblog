80ca230483820c4cc64e78aa7753f270822e7c9f
zhongtianshan <pythonishelloword@outlook.com>
2026-07-24 17:07:22 +0800

    更新索引

--- data.js
+++ data.js
@@ -1,10 +1,19 @@
 var blogData = {
-  "posts": [],
+  "posts": [
+    {
+      "title": "测试",
+      "date": "未知日期",
+      "tags": [],
+      "file": "测试.md",
+      "content": "!\\[链接](www.baidu.com)(百度)",
+      "preview": "!\\链接(百度)..."
+    }
+  ],
   "images": [
     {
       "name": "山水.jpg",
       "path": "images/yingxiang/山水.jpg"
     }
   ],
-  "updated": "2026-07-24T17:00:36.301165"
+  "updated": "2026-07-24T17:07:22.262394"
 };
